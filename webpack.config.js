52ba6869329a88219bc6906a695810eb083d7f01
Encantos <encantospastry@gmail.com>
2026-08-17 06:43:18 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "code.js",
+  },
+
+  target: "web",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: false,
+  },
+};
